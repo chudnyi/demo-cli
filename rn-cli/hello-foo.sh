1e483ddfdb5dba9fe106906e7296d22ee910ea50
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2023-07-30 17:55:27 +0300

    feat(tea): Add local tea.yaml

---
+++ rn-cli/hello-foo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env tea -E bash
+
+echo "Use bash version configured in root tea.yaml"
+bash --version
--- rn-cli/rm-mode_modules.sh
+++ rn-cli/rm-mode_modules.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S tea -E bash@5.2.15
 
 echo "This script will remove node_modules folders..."
+bash --version
 
 if [ -f "node_modules" ]; then
   echo "Есть подсветка синтаксиса bash"
